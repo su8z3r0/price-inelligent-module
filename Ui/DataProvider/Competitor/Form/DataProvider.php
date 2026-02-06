a075daed2acd0fd64db66e01ea01b6ad220a57a0
su8z3r0 <ciroperna@libero.it>
2026-02-06 16:25:27 +0100

    Refactor: Standardize Competitor DataProvider structure to match Suppliers

---
+++ Ui/DataProvider/Competitor/Form/DataProvider.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace Cyper\PriceIntelligent\Ui\DataProvider\Competitor\Form;
+
+use Magento\Ui\DataProvider\AbstractDataProvider;
+use Cyper\PriceIntelligent\Model\ResourceModel\Competitor\CollectionFactory;
+use Magento\Framework\App\RequestInterface;
+
+class DataProvider extends AbstractDataProvider
+{
+    protected $loadedData;
+    protected $request;
+
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        RequestInterface $request,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $collectionFactory->create();
+        $this->request = $request;
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+
+    public function getData()
+    {
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+
+        $items = $this->collection->getItems();
+        foreach ($items as $competitor) {
+            $this->loadedData[$competitor->getId()] = $competitor->getData();
+        }
+
+        // Return empty array for new records
+        if (!$this->loadedData) {
+            $this->loadedData = [];
+        }
+
+        return $this->loadedData;
+    }
+}
